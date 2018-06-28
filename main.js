616ab08bfbe87944edcc876a16055c6c3b2a2440
leahgwin <leahgwin@gmail.com>
2018-06-28 13:44:56 -0500

    printed all of the data to the dom in their own sections. Then started the process of making them into cards.

--- main.js
+++ main.js
@@ -6,44 +6,87 @@
 // Once your data is normalized, use your DOM skills to display a card for each employee. It should display the employee name, the name of their department, and which computer they are using
 
 // Build arrays of objects that represent Employees, Departments, and Computers.
-const Database = {
-    "employees": [
-        {
-            // Assign every resource a unique id property.
-            "employeeId": 1,
-            "name": "Seth"
-        },
-        {
-            "employeeId": 2,
-            "name": "Matt"
-        },
-        {
-            "employeeId": 3,
-            "name": "Sarah"
+const employee = [
+    {
+        // Assign every resource a unique id property.
+        "employeeId": 1,
+        "name": "Seth",
+        "deptId": 3,
+        "compId": 2
+    },
+    {
+        "employeeId": 2,
+        "name": "Matt",
+        "deptId": 2,
+        "compId": 1
+    },
+    {
+        "employeeId": 3,
+        "name": "Sarah",
+        "deptId": 1,
+        "compId": 3
+    }
+]
+// console.log(employee);
+
+const departments = [
+    {
+        "deptId": 1,
+        "deptName": "Marketing"
+    },
+    {
+        "deptId": 2,
+        "deptName": "Software Engineering"
+    },
+    {
+        "deptId": 3,
+        "deptName": "Sales"
+    }
+]
+const computers = [
+    {
+        "compId": 1,
+        "compName": "Macbook"
+    },
+    {
+        "compId": 2,
+        "compName": "Chromebook"
+    },
+    {
+        "compId": 3,
+        "compName": "Mac Desktop"
+    }
+]
+
+// Once your data is normalized, use your DOM skills to display a card for each employee. It should display the employee name, the name of their department, and which computer they are using
+
+//
+let company = $("#company");
+
+for (let i = 0; i < employee.length; i++) {
+    let div = $("<div>")
+    let employeeHeader = $("<h2>");
+    employeeHeader.text(employee[i].name)
+    company.append(employeeHeader);
+    company.append(div);
+    let departmentId = employee[i].deptId;
+    for (let i = 0; i < departments.length; i++) {
+        if (departmentId === departments[i].deptId) {
+            let deptSect = $("<li>");
+            deptSect.text(departments[i].deptName);
+            div.append(deptSect);
+            div.attr("id", "dept")
+
         }
-    ],
-    "departments": [
-        {
-            "deptId": 1,
-            "name": "Marketing"
-        },
-        {
-            "deptId": 2,
-            "name": "Software Engineering"
-        },
-        {
-            "deptId": 3,
-            "name": "Sales"
+    }
+    let computerId = employee[i].compId;
+    for (let i = 0; i < computers.length; i++) {
+        if (computerId === computers[i].compId) {
+            let compSect = $("<li>");
+            compSect.text(computers[i].compName);
+            div.append(compSect);
+            div.attr("id", "comp")
         }
-    ],
-    "computers": [
-        {
-            "compId": 1,
-            "name": "Macbook"
-        },
-        {
-            "compId": 2,
-            "name": "Chromebook"
-        }
-    ]
+    }
+
 }
